527b15c9796336d0e5072cd75ef1ca9e49c48bab
Tzu-Chi Yen <tzuchi.yen@colorado.edu>
2023-12-04 18:30:09 -0700

    added DandiHandler

--- functional_connectivity/io.py
+++ functional_connectivity/io.py
@@ -17,3 +17,35 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from dandi.dandiapi import DandiAPIClient
+from pynwb import NWBHDF5IO
+
+
+class DandiHandler():
+    def __init__(self, dandiset_id, filepath):
+        self.dandiset_id = dandiset_id
+        self.filepath = filepath
+        self.asset = None
+        self.s3_url = None
+        self.io = None
+        self.nwbfile = None
+
+    def get_s3_url(self):
+        dandiset_id = self.dandiset_id  # PPC_Finger: human posterior parietal cortex recordings during attempted finger movements
+        filepath = self.filepath  # 1.5GB file
+        with DandiAPIClient() as client:
+            self.asset = client.get_dandiset(dandiset_id, "draft").get_asset_by_path(filepath)
+            self.s3_url = self.asset.get_content_url(follow_redirects=1, strip_query=True)
+        return self.s3_url        
+
+    def download(self):
+        if self.s3_url is None:
+            self.get_s3_url()
+        if self.io is None:
+            self.io = NWBHDF5IO(self.s3_url, mode="r", load_namespaces=True, driver='ros3')
+        
+
+    def read(self):
+        self.nwbfile = self.io.read()
+        return self.nwbfile
