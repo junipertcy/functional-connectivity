191cec2ef947f7591b155129eda0ad230d28621b
Tzu-Chi Yen <tzuchi.yen@colorado.edu>
2024-08-05 00:27:07 -0600

    switch build system to poetry

--- functional_connectivity/__init__.py
+++ functional_connectivity/__init__.py
@@ -16,6 +16,15 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
+import importlib as _importlib
+import os
+
+from functional_connectivity.generators import *
+from functional_connectivity.readwrite import *
+from functional_connectivity.inference import *
+from functional_connectivity.stats import *
+from functional_connectivity.utils import *
+
 __package__ = "functional_connectivity"
 __title__ = "functional_connectivity: sensing the functional connectivity of the brain"
 __description__ = ""
@@ -26,21 +35,45 @@ __author__ = """\n""".join(
         "Tzu-Chi Yen <tzuchi.yen@colorado.edu>",
     ]
 )
+__release__ = "0.1.0"
 __URL__ = "https://github.com/junipertcy/functional-connectivity"
-__version__ = "0.1.1"
-__release__ = "0.1"
+submodules = ["generators", "readwrite", "inference", "stats", "utils"]
 
-
-__all__ = [
+dunder = [
+    "__version__",
+    "__package__",
+    "__title__",
+    "__description__",
     "__author__",
     "__URL__",
-    "__version__",
-    "__copyright__",
+    "__license__",
+    "__release__",
 ]
+__all__ = (
+    submodules
+    + [
+        "show_config",
+    ]
+    + dunder
+)
 
-from functional_connectivity import generators, inference, readwrite, stats, utils
-from functional_connectivity.generators import *
-from functional_connectivity.inference import *
-from functional_connectivity.readwrite import *
-from functional_connectivity.stats import *
-from functional_connectivity.utils import *
+
+def __dir__():
+    return __all__
+
+
+def __getattr__(name):
+    if name in submodules:
+        return _importlib.import_module(f"functional_connectivity.{name}")
+    else:
+        try:
+            return globals()[name]
+        except KeyError as err:
+            raise AttributeError(
+                f"Module 'functional_connectivity' has no attribute '{name}'"
+            ) from err
+
+
+def show_config():
+    """Show ``functional_connectivity`` build configuration."""
+    print("uname:", " ".join(os.uname()))
